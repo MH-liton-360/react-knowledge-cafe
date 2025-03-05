652d0794a1e8329ab9dc6ccdcf306f3bb85d946f
MH-liton-360 <liton72524nk@gmail.com>
2025-03-05 15:22:13 +0600

    secod

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Blogs from './Components/Blogs/blogs';
+import Bookmarks from './Components/Bookmarks/Bookmarks';
+import Blog from './Components/Blog/Blog';
 
 const App = () => {
   return (
     <div>
-      <Header></Header>
-      <Blogs></Blogs>
+      <Header></Header>\
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+        <Blog></Blog>
+      </div>
     </div>
   );
 };
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,14 @@
+//import files
+import profile from '../../assets/images/profile.png'
+
+
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-6xl bg-amber-800'>Knowledge Cafe</h1>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
 
-        </div>
+        </header>
     );
 };
 
